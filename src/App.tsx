d52bdd7cb32715fce733a6f61e2ca434c77577ad
Madden Crispin <maddenc0922@gmail.com>
2026-01-29 17:03:23 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,33 @@ import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 
-type Page = 'home' | 'services' | 'about' | 'contact';
+type Page = 'home' | 'services' | 'about' | 'contact' | 'privacy';
 
 function App() {
   const [currentPage, setCurrentPage] = useState<Page>('home');
 
   const renderPage = () => {
-    switch (currentPage) {
-      case 'home':
-        return <HomePage onNavigate={setCurrentPage} />;
-      case 'services':
-        return <ServicesPage />;
-      case 'about':
-        return <AboutPage />;
-      case 'contact':
-        return <ContactPage />;
-      default:
-        return <HomePage onNavigate={setCurrentPage} />;
-    }
+switch (currentPage) {
+  case 'home':
+    return <HomePage onNavigate={setCurrentPage} />;
+
+  case 'services':
+    return <ServicesPage />;
+
+  case 'about':
+    return <AboutPage />;
+
+  case 'contact':
+    return <ContactPage />;
+
+  case 'privacy':
+    return <PrivacyPolicyPage />;
+
+  default:
+    return <HomePage onNavigate={setCurrentPage} />;
+}
   };
 
   return (
